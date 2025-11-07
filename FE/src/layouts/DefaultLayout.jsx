0386ec7a895672f1385f2862d6245408e8aebbda
Junhyeok <cjh64866@naver.com>
2025-11-07 18:35:54 +0900

    feat: DefaultLayout Header 제거

--- FE/src/layouts/DefaultLayout.jsx
+++ FE/src/layouts/DefaultLayout.jsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import DefaultNavBar from "../components/DefaultNavBar";
 
 const DefaultLayout = () => {
   return (
     <main className="w-screen min-h-screen mx-auto">
-      <DefaultNavBar />
       <Outlet />
     </main>
   );
