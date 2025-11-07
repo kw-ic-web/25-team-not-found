0d50d6dea0efb84028add63f702e0c97eba549b9
Junhyeok <cjh64866@naver.com>
2025-11-07 18:36:34 +0900

    feat: StudentSidebar 구현

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout";
+import StudentMain from "./pages/student/StudentMain";
 
 const router = createBrowserRouter([
   {
@@ -9,6 +10,10 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <h1>Home</h1>,
+      },
+      {
+        path: "student",
+        element: <StudentMain />,
       },
     ],
   },
---
+++ FE/src/pages/student/StudentMain.jsx
@@ -0,0 +1,11 @@
+import StudentSidebar from "../../components/sidebar/StudentSidebar";
+
+const StudentMain = () => {
+  return (
+    <main className="w-full h-full">
+      <StudentSidebar />
+    </main>
+  );
+};
+
+export default StudentMain;
