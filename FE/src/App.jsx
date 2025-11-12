14176502b1548149bf87d6d2db8d44adf45bd51c
Junhyeok <cjh64866@naver.com>
2025-11-12 15:31:32 +0900

    refactor: DefaultLayout 원복

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,20 +1,20 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout";
 import StudentMain from "./pages/student/StudentMain";
-import NavBarLayout from "./layouts/NavBarLayout";
 import QuizSolve from "./pages/student/QuizSolve";
 import EnterClass from "./pages/student/EnterClass";
 import StudentDashboard from "./pages/student/StudentDashboard";
 import Landing from "./pages/Landing";
 import RoleSelect from "./pages/RoleSelect";
+import DefaultLayoutV2 from "./layouts/DefaultLayoutV2";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <DefaultLayout />,
+    element: <DefaultLayoutV2 />,
     children: [
       {
-        element: <NavBarLayout />,
+        element: <DefaultLayout />,
         children: [
           {
             index: true,
@@ -38,7 +38,7 @@ const router = createBrowserRouter([
             element: <StudentDashboard />,
           },
           {
-            element: <NavBarLayout />,
+            element: <DefaultLayout />,
             children: [
               {
                 path: "quiz",
--- FE/src/layouts/DefaultLayout.jsx
+++ FE/src/layouts/DefaultLayout.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import DefaultNavBar from "../components/DefaultNavBar";
 
 const DefaultLayout = () => {
   return (
-    <main className="w-screen min-h-screen mx-auto">
-      <Outlet />
-    </main>
+    <>
+      <DefaultNavBar />
+      <div className="w-screen min-h-screen mx-auto">
+        <Outlet />
+      </div>
+    </>
   );
 };
 
