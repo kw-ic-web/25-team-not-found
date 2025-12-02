6d57415a9a8f1906d2451ba2c5ad62a6b6332ff5
Soowon Chang <soowon501@naver.com>
2025-12-03 00:12:12 +0900

    feat: fix URL

--- FE/src/pages/Login.jsx
+++ FE/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://223.194.46.67:21090";
+  import.meta.env.VITE_API_BASE_URL || "https://team10-api.kwweb.org";
 
 export default function Login() {
   const navigate = useNavigate();
